a9e84058f1e3b17579a00aefea5c7790ad2f4726
Arindam <paul.arindam205@gmail.com>
2024-09-25 05:48:14 +0530

    cors issue resolved

--- src/main/java/com/Test1RorApplication/RORApplicationTesting/Config/SecurityConfig.java
+++ src/main/java/com/Test1RorApplication/RORApplicationTesting/Config/SecurityConfig.java
@@ -16,6 +16,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
 
 @Configuration
 @EnableWebSecurity
@@ -29,6 +35,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
         return http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(authorizeConfig -> {
                     authorizeConfig.requestMatchers("/api/v1/auth/**").permitAll();
                     authorizeConfig.requestMatchers("/error").permitAll();
